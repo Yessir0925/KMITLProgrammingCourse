ae4a7c70f440f739579e14198e60b9863dbc1fe7
Ethan <liongethanex@gmail.com>
2025-09-19 10:36:02 +0700

    ch5_5

--- Day 5/ch5_5.2.py
+++ Day 5/ch5_5.2.py
@@ -7,3 +7,17 @@ a-b-c-d-e-f-g-h-i-j-k-l-m-n-o-p-q-r-s-t-u-v-w-x-y-z
 
 print(" *** Alphabet Sequence (a-z) ***")
 FL, SL, RS = input("Enter character step length : ").split()
+STRBUILD = ""
+
+FL = ord(FL)
+RS, SL = int(RS), int(SL)
+
+for i in range(RS):
+    if i != 0:
+        STRBUILD = STRBUILD + "-" + str(chr(FL))
+    else:
+        STRBUILD = str(chr(FL))
+    FL += SL
+
+print(STRBUILD)
+print("===== End of program =====")
