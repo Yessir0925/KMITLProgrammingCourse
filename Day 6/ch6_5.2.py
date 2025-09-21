488577b6062d302931fa62a68a4aa98a42de8aae
Yessir <lilnogger2000@gmail.com>
2025-09-21 13:30:31 +0700

    ch6.5

--- Day 6/ch6_5.2.py
+++ Day 6/ch6_5.2.py
@@ -13,25 +13,35 @@ w, h = input("Enter width height : ").split()
 w, h = int(w), int(h)
 
 NumList = []
+
 for numListInsertion in range(1,(w*h)+1,+1):
     NumList.append(numListInsertion)
 
-DLMatrix = [[],[]]
-#[Column],[Row]
+NumList = NumList[::-1]
 
-#Set horPointer to end of column
-#Set vertPointer to top of row
+DLMatrix = [[None] * w for _ in range(h)]
 
-#increment vertPointer till finish set
-#Minus horPointer and go to top then repeat
+CounterNumList = 0
+for hDP in range(h):
+    for wDP in range(w):
+        DLMatrix[hDP][wDP] = NumList[CounterNumList]
+        CounterNumList += 1
 
-"""
-1. Sort pointers
-2. Figure out flipping
-    1. If end of then break? and all in same loop
-"""
+CounterNumList = 0
+for slicePointer in range(len(DLMatrix)):
+    if slicePointer % 2 == 1:
+        DLMatrix[slicePointer] = (DLMatrix[slicePointer])[::-1]
+    for IncrementRowPointer in range(h):
+        NumList[CounterNumList] = DLMatrix[slicePointer][IncrementRowPointer]
+        CounterNumList += 1
 
-NumListPointer = 0
-for vP in range(h):
-    for hP in range(w):
-        
+CounterNumList = 0
+for hDP in range(h):
+    for wDP in range(w):
+        DLMatrix[wDP][hDP] = NumList[CounterNumList]
+        CounterNumList += 1
+
+for row in DLMatrix:
+    print(" ".join(f"{x:>{w}}" for x in row))
+
+print("===== End of program =====")
