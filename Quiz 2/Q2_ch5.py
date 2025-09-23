1ce63b72e8c5c49aa96b5ead7e6bd3bc279a090f
Ethan <liongethanex@gmail.com>
2025-09-23 09:54:00 +0700

    stuk

---
+++ Quiz 2/Q2_ch5.py
@@ -0,0 +1,41 @@
+"""
+ *** Spiral Matrix ***
+Enter width height : 4 4
+01 02 03 04 
+12 13 14 05 
+11 16 15 06 
+10 09 08 07 
+===== End of program =====
+"""
+
+print(" *** Spiral Matrix ***")
+w, h = input("Enter width height : ").split()
+w, h = int(w), int(h)
+
+numList = [i for i in range(w*h)]
+
+#Go to end max, Go Down max, Go to start min, Go up min
+
+"""
+[0][0] [0][1] [0][2] [0][3]
+[1][0] [1][1] [1][2] [1][3]
+[2][0] [2][1] [2][2] [2][3]
+[3][0] [3][1] [3][2] [3][3]
+
+[0][0] [0][1] [0][2] [0][3] | [1][3] [2][3] [3][3] | [3][2] 
+[3][1] | [3][0] [2][0] [1][0] | [1][1] [1][2] | [2][2] [2][1]
+    #[0][0,1,2,3]
+    #[1,2,3][3]
+    #[3][2,1,0]
+    #[2,1][0]
+    #[1][1,2]
+    #[2][2]
+    #[2][1]
+"""
+
+
+DLMatrix =  [[w*r + c for c in range(w)] for r in range(h)]
+
+
+for rows in DLMatrix:
+    print(" ".join(str(columns) for columns in rows))
