f39c664d355ac2d27e5fc385cff0c9abe111de31
Yessir <lilnogger2000@gmail.com>
2025-09-25 15:43:08 +0700

    ch6.5

--- Day 6/ch6_5.py
+++ Day 6/ch6_5.py
@@ -1,19 +1,32 @@
-print(" *** Rectangle down-left-up-left ***")
-w, h = map(int, input("Enter width height : ").split())
+"""
+*** Rectangle down-left-up-left ***
+Enter width height : 4 3
+12  7  6  1
+11  8  5  2
+10  9  4  3
+===== End of program =====
+"""
 
-DLMatrix = [[0]*w for _ in range(h)]
-val = w*h
-for c in range(w):
-    if c % 2 == 0:                 # even column: top -> bottom
-        for r in range(h):
-            DLMatrix[r][c] = val
-            val -= 1
-    else:                           # odd column: bottom -> top
-        for r in range(h-1, -1, -1):
-            DLMatrix[r][c] = val
-            val -= 1
+print("*** Rectangle down-left-up-left ***")
+w,h = map(int,input("Enter width height : ").split())
 
-field = len(str(w*h))
-for row in DLMatrix:
-    print(" ".join(f"{x:>{field}}" for x in row))
+Value = 1
+DLMatrix = [[None]*w for _ in range(h)]
+
+
+Switch = False
+for c in range(w-1,-1,-1):
+    if Switch == False:
+        for r in range(0,h,+1):
+            DLMatrix[r][c] = Value
+            Value += 1
+    else:
+        for r in range(h-1,-1,-1):
+            DLMatrix[r][c] = Value
+            Value += 1
+    Switch = not Switch
+
+for rows in DLMatrix:
+    print(" ".join(f"{pw:>{w}}" for pw in rows))
+
 print("===== End of program =====")
