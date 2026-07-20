f534727c8caa7cef7feb5e5913ffe5df4b88503c
Yessir <lilnogger2000@gmail.com>
2026-07-20 13:04:41 +0700

    Case fix ch4.1

--- Y2/S4/ch4_1.py
+++ Y2/S4/ch4_1.py
@@ -9,54 +9,62 @@ After dequeueing.
 
 ***Finally, if there is still a value in the queue, display it. If there is no value, display "Empty".*** 
 
-Enter Input : E 10,E 20,E 30,E 40,D,D
+Enter Input : E 10,E 20,E 30,E 40,D,E 50,E 60,D,D,D,D,D,D
 Add 10 index is 0
 Add 20 index is 1
 Add 30 index is 2
 Add 40 index is 3
 Pop 10 size in queue is 3
-Pop 20 size in queue is 2
-Number in Queue is :  ['30', '40']
+Add 50 index is 3
+Add 60 index is 4
+Pop 20 size in queue is 4
+Pop 30 size in queue is 3
+Pop 40 size in queue is 2
+Pop 50 size in queue is 1
+Pop 60 size in queue is 0
+-1
+Empty
 """
 
 usrinp = input("Enter Input : ")
-usrinplist = [x for x in usrinp.split(',')]
+usrinplist = [x.strip() for x in usrinp.split(',') if x.strip()]
+
 
 class Queue:
     def __init__(self):
         self.queue = []
         self.counter = 0
-    
-    def runtasks(self, tasklist):
-        for i in range(len(tasklist)):
-            try:
-                if tasklist[0] == "D":
-                    self.dequeue()
-                elif (tasklist[0].split())[0] == "E":
-                    self.enqueue(tasklist)
-            except Exception as e:
-                print("Error: ", e)
 
-    def enqueue(self, tasklist):
-        if tasklist:
-            taskrun = (tasklist[0].split())[1]
-            tasklist.pop(0)
-            self.queue.append(taskrun)
-            print(f"Add {taskrun} index is {self.counter}")
-            self.counter += 1
-        else:
-            print("Empty")
-    
+    def runtasks(self, tasklist):
+            while tasklist:
+                task = tasklist.pop(0)
+                if task == "D":
+                    self.dequeue()
+                elif task.startswith("E"):
+                    value = task.split()[1]
+                    self.enqueue(value)
+
+    def enqueue(self, value):
+        self.queue.append(value)
+        print(f"Add {value} index is {self.counter}")
+        self.counter += 1
+
     def dequeue(self):
         if self.queue:
+            value = self.queue.pop(0)
             self.counter -= 1
-            print(f"Pop {self.queue.pop(0)} size in queue is {self.counter}")
+            print(f"Pop {value} size in queue is {self.counter}")
         else:
-            print("Empty")
+            print("-1")
 
     def __str__(self):
         return f"Number in Queue is :  {self.queue}"
 
+
 NewQueue = Queue()
 NewQueue.runtasks(usrinplist)
-print(NewQueue)
+
+if NewQueue.queue:
+    print(NewQueue)
+else:
+    print("Empty")
