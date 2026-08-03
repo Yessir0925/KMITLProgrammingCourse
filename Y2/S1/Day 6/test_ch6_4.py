e765b17734945ce55255b0a313860e0f0fbd18b3
bobbie <liongethanex02@gmail.com>
2026-08-03 14:09:00 +0700

    init test case ch5_4-5

---
+++ Y2/S1/Day 6/test_ch6_4.py
@@ -0,0 +1,141 @@
+"""Enter Input : 3
+|  |  |
+1  |  |
+2  |  |
+3  |  |
+move 1 from  A to C
+|  |  |
+|  |  |
+2  |  |
+3  |  1
+move 2 from  A to B
+|  |  |
+|  |  |
+|  |  |
+3  2  1
+move 1 from  C to B
+|  |  |
+|  |  |
+|  1  |
+3  2  |
+move 3 from  A to C
+|  |  |
+|  |  |
+|  1  |
+|  2  3
+move 1 from  B to A
+|  |  |
+|  |  |
+|  |  |
+1  2  3
+move 2 from  B to C
+|  |  |
+|  |  |
+|  |  2
+1  |  3
+move 1 from  A to C
+|  |  |
+|  |  1
+|  |  2
+|  |  3
+
+
+Enter Input : 4
+|  |  |
+1  |  |
+2  |  |
+3  |  |
+4  |  |
+move 1 from  A to B
+|  |  |
+|  |  |
+2  |  |
+3  |  |
+4  1  |
+move 2 from  A to C
+|  |  |
+|  |  |
+|  |  |
+3  |  |
+4  1  2
+move 1 from  B to C
+|  |  |
+|  |  |
+|  |  |
+3  |  1
+4  |  2
+move 3 from  A to B
+|  |  |
+|  |  |
+|  |  |
+|  |  1
+4  3  2
+move 1 from  C to A
+|  |  |
+|  |  |
+|  |  |
+1  |  |
+4  3  2
+move 2 from  C to B
+|  |  |
+|  |  |
+|  |  |
+1  2  |
+4  3  |
+move 1 from  A to B
+|  |  |
+|  |  |
+|  1  |
+|  2  |
+4  3  |
+move 4 from  A to C
+|  |  |
+|  |  |
+|  1  |
+|  2  |
+|  3  4
+move 1 from  B to C
+|  |  |
+|  |  |
+|  |  |
+|  2  1
+|  3  4
+move 2 from  B to A
+|  |  |
+|  |  |
+|  |  |
+|  |  1
+2  3  4
+move 1 from  C to A
+|  |  |
+|  |  |
+|  |  |
+1  |  |
+2  3  4
+move 3 from  B to C
+|  |  |
+|  |  |
+|  |  |
+1  |  3
+2  |  4
+move 1 from  A to B
+|  |  |
+|  |  |
+|  |  |
+|  |  3
+2  1  4
+move 2 from  A to C
+|  |  |
+|  |  |
+|  |  2
+|  |  3
+|  1  4
+move 1 from  B to C
+|  |  |
+|  |  1
+|  |  2
+|  |  3
+|  |  4
+
+
+"""
---
+++ Y2/S1/Day 6/test_ch6_5.py
@@ -0,0 +1,45 @@
+""" *** Stair case ***
+Enter Input : 3
+__#
+_##
+###
+===== End of program =====
+
+
+ *** Stair case ***
+Enter Input : 7
+______#
+_____##
+____###
+___####
+__#####
+_######
+#######
+===== End of program =====
+
+
+ *** Stair case ***
+Enter Input : -8
+########
+_#######
+__######
+___#####
+____####
+_____###
+______##
+_______#
+===== End of program =====
+
+
+ *** Stair case ***
+Enter Input : 2
+_#
+##
+===== End of program =====
+
+
+ *** Stair case ***
+Enter Input : 0
+Not Draw!
+===== End of program =====
+"""
